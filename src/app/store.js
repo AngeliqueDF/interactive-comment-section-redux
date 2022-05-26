ba3ba8362cde9fcff880412e6be83823439325b8
angelique-df <faye.angelique.d@gmail.com>
2022-05-26 22:50:56 +0000

    Update store.js

--- src/app/store.js
+++ src/app/store.js
@@ -1,4 +1,12 @@
-import counterReducer from '../features/counter/counterSlice';
+import { configureStore } from "@reduxjs/toolkit";
+import commentsReducer from "../features/comments/commentsSlice";
+import currentUserReducer from "../features/currentUser/currentUserSlice";
+
+// TODO apply store enhancer to add gettingReply, gettingUpdated
 
 export const store = configureStore({
+	reducer: {
+		currentUser: currentUserReducer,
+		comments: commentsReducer,
+	},
 });
