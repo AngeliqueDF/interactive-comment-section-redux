5b5538e309f28278160e24c5e559e084aa776602
angelique-df <faye.angelique.d@gmail.com>
2022-05-31 22:32:00 +0000

    Update CommentContent.js
     Conditionally add `contenteditable` attribute

--- src/features/comments/components/CommentContent.js
+++ src/features/comments/components/CommentContent.js
@@ -7,12 +7,14 @@ const CommentContent = ({
 }) => {
 	return (
 		<div
-			// TODO add gettingUpdated to condition
-			contentEditable={currentUsername === user.username ? true : false}
 			// Add the .updating class to apply a border to the text area, visible when it loses focus
 			className={`comment-content ${
 				currentUsername === user.username && updating ? "updating" : ""
 			}`}
+			// Check the user is the author of the comment and has clicked on edit button before making the div editable
+			contentEditable={
+				currentUsername === user.username && updating ? true : false
+			}
 			suppressContentEditableWarning={
 				currentUsername === user.username ? true : null
 			}
