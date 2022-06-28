637ed7bb7cd5736e1c0413bb68a450e463d4cc6d
angelique-df <faye.angelique.d@gmail.com>
2022-06-28 21:18:46 +0000

    Fix initialState to be 0 index

--- src/features/comments/commentsSlice.js
+++ src/features/comments/commentsSlice.js
@@ -4,7 +4,7 @@ import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = [
 	{
-		id: 1,
+		id: 0,
 		content:
 			"Impressive! Though it seems the drag feature could be improved. But overall it looks incredible. You've nailed the design and the responsiveness at various breakpoints works really well.",
 		createdAt: "1 month ago",
@@ -13,7 +13,7 @@ const initialState = [
 		replies: [],
 	},
 	{
-		id: 2,
+		id: 1,
 		content:
 			"Woah, your project looks awesome! How long have you been coding for? I'm still new, but think I want to dive into React as well soon. Perhaps you can give me an insight on where I can learn React? Thanks!",
 		createdAt: "2 weeks ago",
