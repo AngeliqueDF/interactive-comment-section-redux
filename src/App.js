a4b51978c7465d1f92fdc78f5215974ed02f893b
angelique-df <faye.angelique.d@gmail.com>
2022-06-29 18:41:31 +0000

    Update App.js
    - Remove selectors (moved to their reducers)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
 import "./App.css";
 
-import { useSelector } from "react-redux";
-
 import CommentList from "./features/comments/components/CommentList";
 import NewComment from "./features/comments/components/NewComment";
 
 function App() {
-	// TODO move to its reducer
-	const selectComments = (state) => state.comments;
-
-	const comments = useSelector(selectComments);
-
-	// TODO move useSelector argument to its reducer
-	const currentUser = useSelector((state) => state.currentUser);
-
 	// TODO fetch all comments in useEffect
 	return (
 		<main>
