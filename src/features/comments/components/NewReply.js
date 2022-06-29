a3e8901c45d14c9efea024944108aa0ad707b0eb
angelique-df <faye.angelique.d@gmail.com>
2022-06-29 18:24:32 +0000

    Create NewReply.js
    Submitting a reply will dispatch commentsSlice/addReply

---
+++ src/features/comments/components/NewReply.js
@@ -0,0 +1,56 @@
+import { useDispatch, useSelector } from "react-redux";
+import { selectCurrentUser } from "./../../currentUser/currentUserSlice";
+
+import { addReply } from "../commentsSlice";
+import { selectUsernames } from "../../users/usersSlice";
+
+import Button from "./Button";
+
+const NewReply = ({ replyingToUser, replyingRef, replyingToComment }) => {
+	const currentUser = useSelector(selectCurrentUser);
+	const usernames = useSelector(selectUsernames);
+	const replyingToAuthor = usernames.find(
+		(username) => username.id === replyingToUser
+	);
+	const dispatch = useDispatch();
+	// If the user's information wasn't received yet, exit
+	if (!currentUser) return;
+
+	const attributes = {
+		dataRequestType: "ADD_COMMENT",
+		formClass: "new-comment reply",
+		defaultValue: `@${replyingToAuthor.username} `,
+		btnContent: "reply",
+	};
+
+	const handleSubmit = (e) => {
+		e.preventDefault();
+		const payload = {
+			content: e.target.newContent.value,
+			user: currentUser.id,
+			replyingToUser,
+			replyingToAuthor,
+			replyingToComment,
+		};
+		dispatch(addReply(payload));
+	};
+
+	return (
+		<form className={attributes.formClass} onSubmit={handleSubmit}>
+			<img src={currentUser.image.png} alt={currentUser.username} />
+			<textarea
+				defaultValue={attributes.defaultValue}
+				ref={!(replyingToUser === null) ? replyingRef : null}
+				name="newContent"
+				required={true}
+			></textarea>
+
+			<Button
+				dataRequestType={attributes.dataRequestType}
+				content={attributes.btnContent}
+			/>
+		</form>
+	);
+};
+
+export default NewReply;
