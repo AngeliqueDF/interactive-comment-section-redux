aad161dcc1251a7d2db78110432d8611997c825e
angelique-df <faye.angelique.d@gmail.com>
2022-06-29 18:30:01 +0000

    Update NewComment.js
    Remove logic for adding a new reply in NewComment

--- src/features/comments/components/NewComment.js
+++ src/features/comments/components/NewComment.js
@@ -1,16 +1,14 @@
 import Button from "./Button";
 
-const NewComment = ({ currentUser, replyingTo, replyingRef }) => {
+const NewComment = (props) => {
 	// If the user's information wasn't received yet, exit
 	if (!currentUser) return;
 
-	// Attributes will have different values depending on whether the button is used to add a reply or a new comment
 	const attributes = {
 		dataRequestType: "ADD_COMMENT",
-		formClass: replyingTo ? "new-comment reply" : "new-comment",
-		defaultValue: replyingTo ? `@${replyingTo} ` : "",
-		placeholder: replyingTo ? "" : "Add a comment…",
-		btnContent: replyingTo ? "reply" : "send",
+		formClass: "new-comment",
+		placeholder: "Add a comment…",
+		btnContent: "send",
 	};
 
 	const handleClick = (e) => {
@@ -24,12 +22,12 @@ const NewComment = ({ currentUser, replyingTo, replyingRef }) => {
 			<textarea
 				defaultValue={attributes.defaultValue}
 				placeholder={attributes.placeholder}
-				ref={replyingTo ? replyingRef : null}
+				name="newContent"
+				required={true}
 			></textarea>
 
 			<Button
 				dataRequestType={attributes.dataRequestType}
-				onClick={handleClick}
 				content={attributes.btnContent}
 			/>
 		</form>
