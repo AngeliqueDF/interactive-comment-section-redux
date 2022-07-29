c0cd7c6af8c12c6faea0a1dbf3bf24b075f93ef0
angelique-df <faye.angelique.d@gmail.com>
2022-07-29 20:52:38 +0000

    Import icons for buttons

--- src/features/comments/components/CommentControl.js
+++ src/features/comments/components/CommentControl.js
@@ -1,6 +1,11 @@
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../currentUser/currentUserSlice";
 
+// Import icons
+import deleteIcon from "./../../../images/icon-delete.svg";
+import editIcon from "./../../../images/icon-edit.svg";
+import replyIcon from "./../../../images/icon-reply.svg";
+
 /**
  * Presentational comment to display button element.
  */
