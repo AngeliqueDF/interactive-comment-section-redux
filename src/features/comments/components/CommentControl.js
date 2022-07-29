625edcf286bc7890aca3a4592224faa173bc6b25
angelique-df <faye.angelique.d@gmail.com>
2022-07-29 20:49:43 +0000

    Remove code before refactor

--- src/features/comments/components/CommentControl.js
+++ src/features/comments/components/CommentControl.js
@@ -10,39 +10,6 @@ const CommentControl = ({
 	setReplyingToComment,
 }) => {
 	const currentUser = useSelector(selectCurrentUser);
-	if (currentUser.id === authorID) {
-		const handleEditBtnClick = () => {
-			toggleUpdating();
-		};
-		const handleDeleteBtnClick = () => {
-			toggleDeleteModal(commentID);
-			console.log(commentID);
-		};
-		return (
-			<div className="comment-control">
-				{/* TODO onclick, open modal */}
-				<button onClick={handleDeleteBtnClick} className="delete-button">
-					Delete
-				</button>
-				{/* TODO onclick, change gettingUpdated state and make content editable */}
-				<button onClick={handleEditBtnClick} className="edit-button">
-					Edit
-				</button>
-			</div>
-		);
-	} else {
-		const handleClick = (e) => {
-			toggleGettingReply();
-			setReplyingToComment(commentID);
-		};
-		return (
-			<div className="comment-control">
-				<button onClick={handleClick} className="reply-button">
-					Reply
-				</button>
-			</div>
-		);
-	}
 };
 
 export default CommentControl;
