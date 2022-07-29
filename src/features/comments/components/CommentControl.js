35702bdfd6bd85dc8bfaa9bc36765eb94b0fb101
angelique-df <faye.angelique.d@gmail.com>
2022-07-29 20:51:58 +0000

    Add presentational component `ControlButton`

--- src/features/comments/components/CommentControl.js
+++ src/features/comments/components/CommentControl.js
@@ -1,6 +1,24 @@
 import { useSelector } from "react-redux";
 import { selectCurrentUser } from "../../currentUser/currentUserSlice";
 
+/**
+ * Presentational comment to display button element.
+ */
+const ControlButton = ({
+	onClickFunction,
+	className,
+	textContent,
+	iconImage,
+}) => (
+	<button
+		style={{ backgroundImage: iconImage }}
+		onClick={() => onClickFunction()}
+		className={className}
+	>
+		{textContent}
+	</button>
+);
+
 const CommentControl = ({
 	authorID,
 	commentID,
