8c71e6d74e7aaff98ec97f00aae1a637d286e864
angelique-df <faye.angelique.d@gmail.com>
2022-09-13 19:05:22 +0000

    Call the `API` in `useEffect` hook

--- src/features/comments/components/CommentList.js
+++ src/features/comments/components/CommentList.js
@@ -1,11 +1,16 @@
-import React from "react";
-import { useSelector } from "react-redux";
-import { selectComments } from "./../commentsSlice";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { selectComments, getAllComments } from "./../commentsSlice";
 
 import TopLevelComment from "./TopLevelComment";
 
 const CommentList = () => {
 	const comments = useSelector(selectComments);
+	const dispatch = useDispatch();
+	useEffect(() => {
+		dispatch(getAllComments());
+	}, []);
+
 	return (
 		<>
 			{comments &&
