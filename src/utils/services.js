59557c73ee07fea584584c022ea7d7c1b579ef98
angelique-df <faye.angelique.d@gmail.com>
2022-09-14 11:47:44 +0000

    Fix `API_URL` value

--- src/utils/services.js
+++ src/utils/services.js
@@ -1,7 +1,8 @@
 /**
  * A service to send requests to the backend.
  */
-const API_URL = process.env.API_URL || "http://localhost:5000/api/comments/";
+const API_URL =
+	process.env.REACT_APP_API_URL || "http://localhost:5000/api/comments/";
 
 const getAllComments = async () => {
 	const response = await fetch(API_URL);
